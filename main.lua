690f357be2402fc55853ec0ad8407ed3bbe4a816
GoatMyGoat <kushnirpb@gmail.com>
2025-05-11 17:54:44 -0400

    Add files via upload

--- main.lua
+++ main.lua
@@ -4,19 +4,25 @@ local player = require "player"
 function love.load() --loads when game is run 
     print("hiya")
     player.load()
+   
 end
-
 function love.update(dt) --what the game checks every frame the is running.
-    player.update(dt)
-    bullet.update(dt)
+player.update(dt)
+bullet.update(dt)
 end
-
 function love.draw()-- for graphics 
+ local tile =love.graphics.newImage("sprites/tile1.png")
+    for tx=0,1280 ,65 do
+        for ty=0,720,65 do
+        love.graphics.draw(tile,tx,ty)
+        end
+    end
+    love.graphics.draw(love.graphics.newImage("sprites/tower.png"),500,200)
     player.draw()
     bullet.draw()
+   
 end
-
-function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2) -- checks the collision of 2 things
+function CheckCollision(x1,y1,w1,h1, x2,y2,w2,h2)--checks the collision of 2 things
     return x1 < x2+w2 and
            x2 < x1+w1 and
            y1 < y2+h2 and
